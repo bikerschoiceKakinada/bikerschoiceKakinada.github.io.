4933168e7548ac521904f4ab5b5b08e2b70a8ae4
bikerschoiceKakinada <plaground.lab@gmail.com>
2026-02-27 12:57:00 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/your-base-path/',  // Change to your desired base path
+  base: '/',  // Change to your desired base path
   build: {
     outDir: 'dist',  // Set the output directory for builds
     // Additional build options can be specified here
