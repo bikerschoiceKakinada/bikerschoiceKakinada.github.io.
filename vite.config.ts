7054c9b1c2297d4d6d260be488179ab3acfd1749
bikerschoiceKakinada <plaground.lab@gmail.com>
2026-02-27 13:12:42 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/",        // correct base for https://bikerschoicekakinada.github.io
+  base: "/",       // DO NOT CHANGE THIS
   plugins: [react()],
   build: {
     outDir: "dist",
   },
-})
+});
